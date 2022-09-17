965451285299dc292e93e8947de3d2d69e6d303e
Allan Diogo Bastos <allan.diogo.bastos@gmail.com>
2022-09-17 15:44:00 -0300

    primeiro commit

---
+++ src/components/SectionTypesExercises.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1 class="text-4xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+</template>
+<script>
+export default {
+  name: 'SectionTypesExercises',
+  components: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
